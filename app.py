52101c038354d5ddb5db2c746a74e5b8c4a4780d
23CSBSAKTHIVEL <140485625+23BSCCSSAKTHIVEL@users.noreply.github.com>
2026-03-13 10:39:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -262,11 +262,11 @@ def complete_sale():
     save_sales(sales)
 
     return jsonify({"status": "success"})
-port = int(os.environ.get("PORT", 10000))
-app.run(host="0.0.0.0", port=port)
+
 # -------------------------------
 # Run Server
 # -------------------------------
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
